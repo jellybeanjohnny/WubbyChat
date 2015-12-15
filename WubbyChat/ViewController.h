24e12b95091b289035496d844f14332fcb8f5504
Matt Amerige <matt@wubbyland.com>
2015-12-14 21:58:24 -0500

    Initial Commit

---
+++ WubbyChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WubbyChat
+//
+//  Created by Matt Amerige on 12/14/15.
+//  Copyright © 2015 Wubbyland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
