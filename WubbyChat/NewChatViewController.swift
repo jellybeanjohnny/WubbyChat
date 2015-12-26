fd8ff16af2073baa634496eb3ca462406098152e
Matt Amerige <matt@wubbyland.com>
2015-12-25 23:34:33 -0500

    Changing New Group creation to use collectionview
    Summary:
    
    Task ID: #
    
    Reviewers:bdaz

--- WubbyChat/NewChatViewController.swift
+++ WubbyChat/NewChatViewController.swift
@@ -8,29 +8,70 @@
 
 import UIKit
 
-class NewChatViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-  
-  @IBOutlet weak var _tableView: UITableView!
-  @IBOutlet weak var _searchBar: UISearchBar!
-  
+class NewChatViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+  @IBOutlet weak var _groupNameField: UITextField!
+  @IBOutlet weak var _membersCollectionView: UICollectionView!
+
+  var _members : Array<Int>?
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    _membersCollectionView.delegate = self
+    _membersCollectionView.dataSource = self
+    
+    // Setting the members array to an empty array
+    _members = []
   }
   
+  //MARK: - Actions
   
-  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  @IBAction func _cancel(sender: AnyObject) {
+    
+    self.dismissViewControllerAnimated(true, completion: nil);
+    
+  }
+  
+  /**
+   Presents a friend picker for the user to add friends
+   */
+  @IBAction func _addFriends(sender: AnyObject) {
+    print("Add button pressed")
+    _members?.append(1)
+    _membersCollectionView.reloadData()
+  }
+  
+  //MARK: UICollectionView
+  
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return (_members?.count)! + 1
+  }
+  
+  func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
     return 1
   }
   
-  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    return 0
-  }
-  
-  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-   let cell = tableView.dequeueReusableCellWithIdentifier("friendCell", forIndexPath:indexPath)
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    
+    var cellIdentifier: String
+    
+    if indexPath.row == (_members?.count)! {
+      cellIdentifier = "addCell"
+    } else {
+      cellIdentifier = "memberCell"
+    }
+    
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath)
     
     return cell
+  }
+  
+  func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    print("item selected")
+  }
+
+  @IBAction func _removeFriend(sender: AnyObject) {
+    print("friend removed")
   }
   
   
